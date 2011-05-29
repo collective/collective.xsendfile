4abf7c12653defe39bca309e4aab2b67c08c9ef2
Mikko <mikko@mfabrik.com>
2011-05-29 15:57:06 +0300

    Added proper README and setup.py meta info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.xsendfile',
       version=version,
       description="Offload ZODB BLOB download to front end web server using XSendfile protocol",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
