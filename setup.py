2a8edd41880667274cae80d9fa5c47b254e221b9
Peter Holzer <peter.holzer@agitator.com>
2017-05-12 10:05:24 +0200

    update classifiers

--- setup.py
+++ setup.py
@@ -3,43 +3,54 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-setup(name='collective.xsendfile',
-      version=version,
-      description="Offload ZODB BLOB download to front end web server using XSendfile protocol",
-      long_description=open("README.rst").read() + "\n" +
-                       open("HISTORY.txt").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Plone",
+version = '1.1.dev0'
+
+setup(
+    name='collective.xsendfile',
+    version=version,
+    description="Offload ZODB BLOB download to front end web server using "
+                "XSendfile/HTTP-Accel protocol",
+    long_description=(
+        open("README.rst").read() +
+        "\n" +
+        open("CHANGES.rst").read()
+    ),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+      "Programming Language :: Python",
+      "Framework :: Plone",
+      "Framework :: Plone :: 4.3",
+      "Framework :: Plone :: 5.0",
+      "Framework :: Plone :: 5.1",
+
       ],
-      keywords='',
-      author='Mikko Ohtamaa',
-      author_email='mikko@mfabrik.com',
-      url='https://github.com/collective/collective.xsendfile',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'collective.monkeypatcher',
-          'plone.app.registry',
-          'plone.registry',
-          # 'z3c.form',
-      ],
-      extras_require={
-          'test': [
-              'plone.app.testing',
-              'plone.app.robotframework'
-          ],
-      },
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    keywords='',
+    author='Mikko Ohtamaa',
+    author_email='mikko@mfabrik.com',
+    url='https://github.com/collective/collective.xsendfile',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        'collective.monkeypatcher',
+        'plone.app.registry',
+        'plone.registry',
+        # 'z3c.form',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.robotframework'
+        ],
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
