77ae97c1434a46929b4c5abd5dd2479ef2b04257
Jens W. Klein <jk@kleinundpartner.at>
2017-05-12 09:43:05 +0200

    fix buildout

--- bootstrap-5.1.x.sh
+++ bootstrap-5.1.x.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-ln -fs plone-5.0.x.cfg buildout.cfg
+ln -fs plone-5.1.x.cfg buildout.cfg
 rm -r ./lib ./include ./local ./bin
 virtualenv --clear .
 ./bin/pip install -r requirements.txt
