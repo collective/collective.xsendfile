831d994bbb1da681f562ac1ac626bdebe94bef4d
Jon Pentland <jon.pentland@pretagov.co.uk>
2026-07-10 09:52:50 +0100

    Implement CI & add scales tests.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     extras_require={
         'test': [
             'plone.app.testing',
-            'plone.app.robotframework',
+            'zope.testrunner',
             'coverage',
         ],
         'lint': [
